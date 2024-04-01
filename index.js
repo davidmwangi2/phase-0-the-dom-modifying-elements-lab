6363aa7b8db69db08552759425b59c298fd70cd7
david <david.mwangi2@student.moringaschool.com>
2024-04-01 21:41:39 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'David is the champion';
+const mainElement = document.getElementById('main');
+mainElement.appendChild(newHeader);
+mainElement.remove();
